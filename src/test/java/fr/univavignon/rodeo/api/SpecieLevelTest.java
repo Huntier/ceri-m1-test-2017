81fdd3bec32a745cc32f2174765d86eb5a276216
Renaud <renodu84@hotmail.fr>
2018-03-05 18:51:22 +0100

    premier test

---
+++ src/test/java/fr/univavignon/rodeo/api/SpecieLevelTest.java
@@ -0,0 +1,14 @@
+package fr.univavignon.rodeo.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpecieLevelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
