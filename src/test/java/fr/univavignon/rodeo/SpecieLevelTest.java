28eb46a02641a8b2502809ff8d8a97b649c7d23a
Renaud <renodu84@hotmail.fr>
2018-03-14 16:31:45 +0100

    Changement nom package fr.univavignon.rodeo

--- src/test/java/fr/univavignon/rodeo/SpecieLevelTest.java
+++ src/test/java/fr/univavignon/rodeo/SpecieLevelTest.java
@@ -1,14 +1,9 @@
-package fr.univavignon.rodeo.api;
+package fr.univavignon.rodeo;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class SpecieLevelTest {
-
-	@Test
-	public void test() {
-		assertEquals(0,1);
-	}
 
 }
