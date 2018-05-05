cfa00b4cb912c8282c4afd9d19c3c89814920df1
Renaud <renodu84@hotmail.fr>
2018-05-05 12:28:48 +0200

    Ajout implementation de IAnimal, INamedObject, IEnvironment et ISpecie

---
+++ src/main/java/fr/univavignon/rodeo/implementation/NamedObject.java
@@ -0,0 +1,24 @@
+package fr.univavignon.rodeo.implementation;
+
+import fr.univavignon.rodeo.api.INamedObject;
+
+/**
+ * Implementation of INamedObject.
+ * 
+ * @author rg
+ */
+
+
+public class NamedObject implements INamedObject{
+	
+	private String name;
+	
+	public NamedObject(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
